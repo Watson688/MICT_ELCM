9a42b6f906caafb6576002920fe42993f5ae8bf3
ChenWang617 <wangchen9617@gmail.com>
2018-08-27 18:27:40 -0400

    clean the data

--- Src/ELCM/LSTM_TF.py
+++ Src/ELCM/LSTM_TF.py
@@ -11,7 +11,7 @@ class lstm():
     def __init__(self):
         pass
 
-    def tf_lstm(self):
+    def tf_lstm_preprocessing(self):
         # read data
         df_events = pd.read_csv(str(Path(os.path.realpath(__file__)).parent.parent.parent / 'Data/LSTM_Data/events_data_for_RF.csv'), index_col=0)
         df_events = df_events.dropna(axis=0).rename(columns={"DEVICE":"device"})
@@ -20,13 +20,47 @@ class lstm():
         df_merged.sort_values(by=['device','date'], inplace=True)
         agvs = df_merged.device.unique()
         groupby_agv = {}
+        x_abnormal = []
+        x_normal =[]
+        size_of_input = 10
         print("grouping")
         for a in agvs:
             groupby_agv[a] = df_merged[df_merged['device'] == a]
-        # iterate over 
+        # iterate over each agv
+        # abnormal
+        print("iterating 1")
         for agv in groupby_agv.keys():
-            for row in groupby_agv[agv]:
-                pass
+            for row in groupby_agv[agv].itertuples(index=True):
+                last_position = None
+                if row.Index - size_of_input + 1 < 0:
+                    continue
+                if row[-1] == 1 and (last_position is None or row.Index - last_position >= size_of_input):
+                    x_abnormal.append(groupby_agv[agv].iloc[row.Index - size_of_input + 1:row.Index+1])
+                    last_position = row.Index
+
+        print("iterating 2")
+        for agv in groupby_agv.keys():
+            start = None
+            for row in groupby_agv[agv].itertuples(index=True):
+                if row[-1] == 0.0:
+                    if not start:
+                        start = row.Index
+                    elif row.Index - start == 9:
+                        x_normal.append(groupby_agv[agv].loc[start:row.Index])
+                        start = None
+                else:
+                    start = None
+        return x_abnormal, x_normal
+    
+    def tf_lstm(self):
+
+        lr = 0.001
+        training_iters = 10000
+        batch_size = 128
+        n_inputs = 9
+
+        x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+        y = tf.placeholder(tf.float32, [None, n_classes])
 
 
 
